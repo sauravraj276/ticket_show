bc14f272089793577618252d5033b99935bdb1cb
Saurav Raj <sauravraj@gmail.com>
2023-03-22 23:02:40 +0530

    model and database defined

---
+++ application/models.py
@@ -0,0 +1,24 @@
+from .database import db
+
+class User(db.Model):
+    __tablename__ = 'user'
+    user_id = db.Column(db.Integer, autoincrement=True, primary_key=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    type = db.Column(db.Integer, nullable=False)
+    
+class Show(db.Model):
+    __tablename__ = 'show'
+    show_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    rating = db.Column(db.Integer)
+    tags = db.Column(db.String)
+    price = db.Column(db.Integer, nullable=False)
+
+class Venue(db.Model):
+    __tablename__ = 'venue'
+    venue_id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    place = db.Column(db.String, nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
